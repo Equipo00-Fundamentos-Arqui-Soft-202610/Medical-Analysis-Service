136363c4bffaf4d0c0cb820290f8039b75d5d42b
VRojas1603 <rojas.vmanuel16@gmail.com>
2026-06-05 23:47:09 -0500

    Add basic configuration

--- MediTrack.MedicalAnalysisService.API/Program.cs
+++ MediTrack.MedicalAnalysisService.API/Program.cs
@@ -1,25 +1,29 @@
+using MediTrack.MedicalAnalysisService.API.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<MedicalAnalysisDbContext>(options =>
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")!));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<MedicalAnalysisDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
